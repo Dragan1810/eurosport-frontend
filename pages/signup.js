8ee9f86578453956df9b10baba0b7914806cfa8c
Dragan <dragan.novakovic@bridgewaterlabs.com>
2019-05-20 11:42:13 +0200

    qqq

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,5 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
-import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
