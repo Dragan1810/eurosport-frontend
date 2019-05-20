56df30226578a12cc913d0d7a135bac9b744ed62
Dragan <dragan.novakovic@bridgewaterlabs.com>
2019-05-20 13:09:21 +0200

    style fix

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
-import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 const theme = {
   red: '#FF0000',
@@ -15,44 +15,46 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: #393939;
+  color: ${props => props.theme.black};
 `;
 
-const GlobalStyle = createGlobalStyle`
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
   @font-face {
     font-family: 'radnika_next';
-    src: url('/static/radnikanext-medium-webfont.woff2')
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
   }
-
   html {
     box-sizing: border-box;
     font-size: 10px;
   }
-
-  *,*:before, *after {
+  *, *:before, *:after {
     box-sizing: inherit;
   }
-
-  body: {
+  body {
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
     line-height: 2;
+    font-family: 'radnika_next';
   }
-
   a {
     text-decoration: none;
-    color: #393939
+    color: ${theme.black};
   }
-
+  button {  font-family: 'radnika_next'; }
 `;
 
 const Page = ({ children }) => (
   <ThemeProvider theme={theme}>
     <>
-      <GlobalStyle />
       <StyledPage>
         <Meta />
         <Header />
